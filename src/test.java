ad21e37a2f9ec8fb344212d98976d9610611f687
Mohit Joshi <mohit.joshi.mj@gmail.com>
2017-11-11 20:20:57 +0530

    tesing

---
+++ src/test.java
@@ -0,0 +1,21 @@
+public class test {   
+   
+   public void start() {
+   	 // gameofthrones valhar = new gameofthrones();
+      // valhar.character = "jon";
+      // valhar.importance = 7;
+      // valhar.prediction();
+      // String outgame = valhar.prediction();
+      // System.out.println(outgame);
+      // System.out.println(valhar.prediction());
+   		Series s = new houseOfCards ();
+   		houseOfCards h = new houseOfCards ();
+   		s.setSeries("test","test"); 
+	   
+}
+public static void main(String[] args) {
+    test prog = new test();
+    prog.start();
+    System.out.println("This is running");
+   }
+}
